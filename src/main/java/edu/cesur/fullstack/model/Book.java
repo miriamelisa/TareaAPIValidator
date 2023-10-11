f4f17b1bba6c4e40d6b93ec984202867853162c8
miriamelisa <miriam.guanolema1@gmail.com>
2023-10-11 20:51:33 +0200

    PRIMMER CAMBIO

---
+++ src/main/java/edu/cesur/fullstack/model/Book.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstack.model;
+
+import java.sql.Date;
+
+public class Book {
+	
+	private int id;
+	private String tittle;
+	private String author;
+	private Date publishedYear;
+	
+
+}
